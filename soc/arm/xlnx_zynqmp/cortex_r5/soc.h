c55daa8e7c0368c0dc874d6eaa5a88abc754d756
Wendy Liang <wendy.liang@xilinx.com>
2018-11-17 23:42:37 -0800

    soc: Add Xilinx ZynqMP SOC configuration files for RPU
    Addd SOC configuration files for Xilinx ZynqMP RPU
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>

---
+++ soc/arm/xlnx_zynqmp/cortex_r5/soc.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2013-2015 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Board configuration macros for the QEMU for arm platform
+ *
+ * This header file is used to specify and describe board-level aspects for
+ * the 'QEMU' platform.
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#include <misc/util.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _ASMLANGUAGE
+
+#include <device.h>
+#include <misc/util.h>
+#include <random/rand32.h>
+
+/* uart configuration settings */
+#endif /* !_ASMLANGUAGE */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SOC__H_ */
